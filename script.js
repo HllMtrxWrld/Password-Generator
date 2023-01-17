1332b84c5981ccf39b8dfb798283b9be30b3c417
Vitaliy Tymchuk <tymchuk94@gmail.com>
2023-01-17 21:17:15 +0000

    The password generator is done. This commit adds all the code

--- script.js
+++ script.js
@@ -89,35 +89,98 @@ var upperCasedCharacters = [
 ];
 
 
-let randomIndex = Math.floor(Math.random() * upperCasedCharacters.length);
-// console.log(random);
 
+document.getElementById("generate").onclick = function() { //connect prompt to the button
+// promp user 
+  let passwordLength = prompt("Please, choose your password length(10-64 characters)");
+  let passwordLength1 = Number(passwordLength);
+  //console.log(typeof passwordLength1);
+  //console.log(passwordLength1);               //some testing
+  let userInputLowercase = confirm("Do you want lowercases in your password?");
 
-// Function to prompt user for password options
-function getPasswordOptions() {
+  let userInputUppercase = confirm("Do you want uppercases in your password?");
 
+  let userInputNumeric = confirm("Do you want numbers in your password?");
+
+  let userInputSpecial = confirm("Do you want special characters in your password?");
+
+  let allCharacters = []; //array for collecting all characters for password
+  if (userInputLowercase == true) {
+    allCharacters = allCharacters.concat(lowerCasedCharacters)
+    };
+  if (userInputUppercase == true) {
+    allCharacters = allCharacters.concat(upperCasedCharacters)
+  };
+  if (userInputNumeric == true) {
+    allCharacters = allCharacters.concat(numericCharacters)
+  };
+  if (userInputSpecial == true) {
+    allCharacters = allCharacters.concat(specialCharacters)
+  };
+  allCharacters.sort(compareRandome);
+  //console.log(allCharacters);   //test
+
+  // function for mix all characters in the array
+  function compareRandome(a, b) {
+  return Math.random() - 0.5;
+  }
+
+  let passwordOut = "";  // creating the variable to containe our password
+
+  for (let i = 0; i < passwordLength1; i++) {
+  passwordOut = passwordOut + allCharacters[i];
+  
+} 
+
+  var passwordText = document.querySelector('#password'); //the varriable to out our password in password area
+
+  passwordText.value = passwordOut;
+  
+  return passwordOut;
 }
 
-// Function for getting a random element from an array
-function getRandom(arr) {
 
-}
 
-// Function to generate password with user input
-function generatePassword() {
 
-}
 
-// Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+
+
+
+
+
+ 
+// //let randomIndex = Math.floor(Math.random() * passwordLength??????);
+
+// // Function to prompt user for password options
+// function getPasswordOptions() {
+
+// }
+
+// // Function for getting a random element from an array
+// function getRandom(arr) {
+
+  
+
+// }
+// // Function to generate password with user input
+// function generatePassword() {
+
+// }
+
+// // Get references to the #generate element
+// var generateBtn = document.querySelector('#generate');
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector('#password');
+
+//   passwordText.value = password;
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener('click', writePassword);
+
---
+++ test.js
@@ -0,0 +1,8 @@
+function gen_password(len){
+    var password = "";
+    var symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!№;%:?*()_+=";
+    for (var i = 0; i < len; i++){
+        password += symbols.charAt(Math.floor(Math.random() * symbols.length));     
+    }
+    return password;
+}
